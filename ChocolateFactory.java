0ddec99ff80b3c862c277566bef3fdcdc89235ff
Ankit Singh <ankitsingh21062002@gmail.com>
2026-03-19 15:08:32 +0530

    Chocolate Factory Task

--- ChocolateFactory.java
+++ ChocolateFactory.java
@@ -1,8 +1,6 @@
 
 import java.util.Scanner;
 
-
-
 /*
  *  A chocolate factory is packing chocolates into the packets. The chocolate packets here 
  * represent an array of N number of integer values. The task is to find the empty packets(0) of 
@@ -25,16 +23,16 @@ public class ChocolateFactory {
         int count = 0;
         for(int i=0; i<n; i++){
             if(arr[i] != 0){
-                arr[count++] = arr[i];
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+                count++;
             }
         }
-        for (int i = count + 1; i < n; i++) {
-            arr[i] = 0;
-        }
+
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
         sc.close();
     }
-    
 }
