e0bb068662cacc9c12eca1f9755350e21f42dc0b
Ankit Singh <ankitsingh21062002@gmail.com>
2026-04-12 23:05:06 +0530

    Minimum Distance Between Three Equal Elements II

---
+++ DSA_Leetcode/MinimumDistanceBetweenThreeEqualElementsII.java
@@ -0,0 +1,27 @@
+package DSA_Leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class MinimumDistanceBetweenThreeEqualElementsII {
+
+    public int minimumDistance(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int n= nums.length;
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        for(int i = 0;i<n;i++){
+            map.computeIfAbsent(nums[i],k -> new ArrayList()).add(i);
+        }
+
+        for(List<Integer> indices : map.values()){
+            if(indices.size() < 3) continue;
+            for(int i = 0;i+2 < indices.size();i++){
+                int dist = 2 * (indices.get(i+2) - indices.get(i));
+                min = Math.min(min,dist);
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+}
